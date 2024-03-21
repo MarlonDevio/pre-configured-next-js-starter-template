88d8adac83d459353ef93909206e4285546d0f45
MarlonHamaekersPXL <mar.hamaekers@gmail.com>
2024-03-21 21:10:36 +0100

    set typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,21 @@ export default function Home() {
     <>
       <main>
         <ModeToggle />
+        <h1 className=''>
+          i am h1 <br /> And you know it
+        </h1>
+        <h2>i am h2</h2>
+        <h3>i am h3</h3>
+        <h4>i am h4</h4>
+        <h5>i am h5</h5>
+        <h6>i am h6</h6>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          similique unde vitae? Repellendus tenetur vero iste nam minima
+          laboriosam labore omnis? Minima numquam eaque consequuntur, vitae aut
+          ducimus vel corrupti! Lorem, ipsum dolor sit amet consectetur
+          adipisicing
+        </p>
       </main>
     </>
   );
